7b24884d0e3d3780d5b99c65ad1f0884dfbc6334
Higor Roberto <h.saposilva@gmail.com>
2019-10-16 14:46:20 -0300

    Correção na ordenação

--- src/main/java/com/fipe/api/model/VeiculoFipe.java
+++ src/main/java/com/fipe/api/model/VeiculoFipe.java
@@ -51,9 +51,31 @@ public class VeiculoFipe {
         for (int i = values.size(); i >= 1; i--) {
             for (int j = 1; j < i; j++) {
                 if (Integer.parseInt(values.get(j - 1).getValor().replaceAll("[^0-9]", "")) > Integer.parseInt(values.get(j).getValor().replaceAll("[^0-9]", ""))) {
-                    String aux = values.get(j).getValor();
+                    VeiculoFipe veiculoFipeAux = values.get(j);
+
                     values.get(j).setValor(values.get(j - 1).getValor());
-                    values.get(j - 1).setValor(aux);
+                    values.get(j).setMarca(values.get(j - 1).getMarca());
+                    values.get(j).setModelo(values.get(j - 1).getModelo());
+                    values.get(j).setAnoModelo(values.get(j - 1).getAnoModelo());
+                    values.get(j).setCombustivel(values.get(j - 1).getCombustivel());
+                    values.get(j).setCodigoFipe(values.get(j - 1).getCodigoFipe());
+                    values.get(j).setMesReferencia(values.get(j - 1).getMesReferencia());
+                    values.get(j).setAutenticacao(values.get(j - 1).getAutenticacao());
+                    values.get(j).setTipoVeiculo(values.get(j - 1).getTipoVeiculo());
+                    values.get(j).setSiglaCombustivel(values.get(j - 1).getSiglaCombustivel());
+                    values.get(j).setDataConsulta(values.get(j - 1).getDataConsulta());
+
+                    values.get(j - 1).setValor(veiculoFipeAux.getValor());
+                    values.get(j - 1).setMarca(veiculoFipeAux.getMarca());
+                    values.get(j - 1).setModelo(veiculoFipeAux.getModelo());
+                    values.get(j - 1).setAnoModelo(veiculoFipeAux.getAnoModelo());
+                    values.get(j - 1).setCombustivel(veiculoFipeAux.getCombustivel());
+                    values.get(j - 1).setCodigoFipe(veiculoFipeAux.getCodigoFipe());
+                    values.get(j - 1).setMesReferencia(veiculoFipeAux.getMesReferencia());
+                    values.get(j - 1).setAutenticacao(veiculoFipeAux.getAutenticacao());
+                    values.get(j - 1).setTipoVeiculo(veiculoFipeAux.getTipoVeiculo());
+                    values.get(j - 1).setSiglaCombustivel(veiculoFipeAux.getSiglaCombustivel());
+                    values.get(j - 1).setDataConsulta(veiculoFipeAux.getDataConsulta());
                 }
             }
         }
